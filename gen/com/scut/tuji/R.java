c5513268c9b7893ed3d145ff8d9c44fcc0b38d6c
tuji2014 <2558456064@qq.com>
2014-03-17 20:55:52 +0800

    SYP

--- gen/com/scut/tuji/R.java
+++ gen/com/scut/tuji/R.java
@@ -64,42 +64,45 @@ public final class R {
         public static final int userinfo_btn=0x7f02002e;
     }
     public static final class id {
-        public static final int account=0x7f080010;
-        public static final int account_input=0x7f080011;
+        public static final int account=0x7f080013;
+        public static final int account_input=0x7f080014;
         public static final int add_friend=0x7f080002;
-        public static final int addtuji=0x7f080004;
-        public static final int autologin=0x7f08001c;
-        public static final int bottom=0x7f080008;
-        public static final int cancel1=0x7f080022;
-        public static final int confirm=0x7f08001f;
-        public static final int labellogin=0x7f08001b;
-        public static final int login=0x7f080016;
+        public static final int addtuji=0x7f080003;
+        public static final int autologin=0x7f08001f;
+        public static final int cancel1=0x7f080025;
+        public static final int channel1=0x7f080007;
+        public static final int channel2=0x7f08000a;
+        public static final int channel3=0x7f08000d;
+        public static final int confirm=0x7f080022;
+        public static final int imageView1=0x7f080008;
+        public static final int imageView2=0x7f08000b;
+        public static final int imageView3=0x7f08000e;
+        public static final int labellogin=0x7f08001e;
+        public static final int login=0x7f080019;
         public static final int login_reback_btn=0x7f080001;
-        public static final int loginhead=0x7f08000f;
-        public static final int mainButton=0x7f080015;
-        public static final int mainpagebtn=0x7f080009;
-        public static final int mainpagetext=0x7f080006;
-        public static final int mainpagexiala=0x7f080007;
-        public static final int male=0x7f080021;
-        public static final int menu_settings=0x7f080023;
-        public static final int name=0x7f08001d;
-        public static final int option=0x7f080018;
-        public static final int password_input=0x7f080014;
-        public static final int progressBar1=0x7f08000d;
-        public static final int pwd=0x7f08001e;
-        public static final int re_head=0x7f08000e;
-        public static final int re_password=0x7f080013;
-        public static final int recommendbtn=0x7f08000a;
-        public static final int register=0x7f080017;
-        public static final int remember=0x7f080019;
-        public static final int rememberpass=0x7f08001a;
-        public static final int scrolllayout=0x7f08000c;
-        public static final int searchtuji=0x7f080005;
-        public static final int sex=0x7f080020;
+        public static final int loginhead=0x7f080012;
+        public static final int mainButton=0x7f080018;
+        public static final int mainpagetext=0x7f080005;
+        public static final int mainpagexiala=0x7f080006;
+        public static final int male=0x7f080024;
+        public static final int menu_settings=0x7f080026;
+        public static final int name=0x7f080020;
+        public static final int option=0x7f08001b;
+        public static final int password_input=0x7f080017;
+        public static final int progressBar1=0x7f080010;
+        public static final int pwd=0x7f080021;
+        public static final int re_head=0x7f080011;
+        public static final int re_password=0x7f080016;
+        public static final int register=0x7f08001a;
+        public static final int remember=0x7f08001c;
+        public static final int rememberpass=0x7f08001d;
+        public static final int searchtuji=0x7f080004;
+        public static final int sex=0x7f080023;
+        public static final int textView1=0x7f080009;
+        public static final int textView2=0x7f08000c;
+        public static final int textView3=0x7f08000f;
         public static final int title=0x7f080000;
-        public static final int top=0x7f080003;
-        public static final int userinfobtn=0x7f08000b;
-        public static final int xiala=0x7f080012;
+        public static final int xiala=0x7f080015;
     }
     public static final class layout {
         public static final int activity_info=0x7f030000;
@@ -116,19 +119,24 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int mainpage=0x7f050004;
         public static final int menu_settings=0x7f050002;
         public static final int password_digits=0x7f050003;
+        public static final int recommend=0x7f050005;
+        public static final int userinfo=0x7f050006;
     }
     public static final class style {
         public static final int AnimHead=0x7f060004;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -150,8 +158,9 @@ public final class R {
         /**  模糊 
          */
         public static final int ImageScale=0x7f060003;
-        /** 背景是否模糊
+        /**  背景是否模糊 
          */
         public static final int ImageloadingDialogStyle=0x7f060002;
+        public static final int main_tab_but_linear=0x7f060005;
     }
 }
